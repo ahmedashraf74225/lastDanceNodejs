b7ea6e7da4a94db0d850054d505980a1cdfb32a2
ahmedashraf74225 <ahmedashraf74225@gmail.com>
2023-04-18 21:56:00 +0200

    second commit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const router = express.Router();
 const User = require("./models/user_model.js");
 const Post = require("./models/post_model.js");
 
-User.hasMany(Post);
-Post.belongsTo(User);
+User.hasMany(Post, { foreignKey: 'UserID' });
+Post.belongsTo(User, { foreignKey: 'UserID' });
 
 app.use(express.json());
 
--- models/post_model.js
+++ models/post_model.js
@@ -2,10 +2,18 @@ const db = require("../config/db.js");
 const { DataTypes } = require("sequelize");
 
 module.exports = db.define("post ", {
-  id: {
+  Id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
-    autoIncrement: true,
+    autoIncrement: true
+  },
+  UserID: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'Id'
+    }
   },
   title: {
     type: DataTypes.STRING(100),
